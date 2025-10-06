2a1f3cf333beee8e618ceb592f6018bcbeb6cdb9
CAS <alejo.hortua3@gmail.com>
2025-10-06 09:29:09 -0500

    Ajuste Comentarios

--- vistas/modulos/sorteo-amigo.php
+++ vistas/modulos/sorteo-amigo.php
@@ -1,11 +1,9 @@
 <div class="container">
-    <!-- Outer Row -->
     <div class="row justify-content-center" style="margin: auto 0;">
         <div class="col-xl-10 col-lg-10 col-md-10">
             <form method="POST">
                 <div class="card o-hidden border-0 shadow-lg my-5">
                     <div class="card-body p-0">
-                        <!-- Nested Row within Card Body -->
                         <div class="row">
                             <div class="col-lg-12">
                                 <div class="p-5">
